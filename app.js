1b7d4ae312c56adb41f694173b81d7ab96de4950
Oyenuga17 <oyenugaoluwaseun78@gmail.com>
2023-05-15 18:19:07 +0100

    modified codes

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const button = document.querySelector(".btn");
 const joke = document.querySelector(".span");
-console.log(joke);
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
 const url = "https://dad-jokes.p.rapidapi.com/random/joke";
 
-const buttonOnClick = (ev) => {
+const buttonOnClick = () => {
   fetch(url, {
     method: "GET",
     headers: {
@@ -21,4 +22,26 @@ const buttonOnClick = (ev) => {
     });
 };
 
+const searchButtonClick = (e) => {
+  e.preventDefault();
+  const value = input.value.toLowerCase();
+
+  fetch(`https://dad-jokes.p.rapidapi.com/joke/search?term=${value}`, {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "07886ec071msh01160e2b79b8b8cp1f116cjsn72a77418b44a",
+      "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      joke.textContent = data.body[0].setup;
+    })
+    .catch((err) => {
+      console.log("error", err);
+    });
+};
+
 button.addEventListener("click", buttonOnClick);
+form.addEventListener("submit", searchButtonClick);
